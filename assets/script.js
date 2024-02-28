1091434272b24e46b1ad5b78faeaee3789098ad2
Jinkc21 <jin.k.choe@gmail.com>
2024-02-28 16:29:28 -0500

    added to js and html

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,62 @@
 const timer = document.getElementById("timer");
 const startBtn = document.getElementById("startBtn")
-const startingMinutes = 5;
-let time = startingMinutes * 60;
+var seconds = 60;
+var score = 0;
 
-startBtn.addEventListener("click", countdown)
+var questions = [
+  {
+    question: "What does CSS stand for?",
+    choices: ["create solid state", "cascading style sheets", "copy single string"],
+    answer: "cascading style sheets",
+  }
+
+]
+
+// console.log(questions[0].question)
+
+startBtn.addEventListener("click", startQuiz)
 
 
 function countdown() {
-  const minutes = Math.floor(time/60);
-  let seconds = time % 60;
-  const timeInterval = setInterval(function() {
-  timer.innerHTML = `${minutes}: ${seconds}`;
-  time--;
+  const timeInterval = setInterval(function () {
+    timer.innerHTML = `${seconds} remaining`;
+    seconds--;
 
-  if (time === 0) {
-    clearInterval(timer);
-   alert("You are out of time!")
-  }
-  },1000)
+    if (seconds === 0) {
+      clearInterval(timeInterval);
+      alert("You are out of time!")
+    }
+  }, 1000)
+
 }
 
+
+function startQuiz() {
+  document.getElementById("main").style.display='none'
+  countdown();
+  showQuiz();
+}
+function showQuiz(){
+  var question = document.createElement("h1")
+  question.textContent= questions[0].question
+  document.getElementById('quiz').appendChild(question)
+for (var i=0; i<questions[0].choices.length; i++){
+  // console.log(questions[0].choices[i])
+  var choicesBtn = document.createElement("button")
+  choicesBtn.textContent= questions[0].choices[i]
+  choicesBtn.onclick = function(event){
+    // console.log("hello")
+    checkAnswer(event)
+  }
+  document.getElementById('quiz').appendChild(choicesBtn)
+}
+
+}
+function checkAnswer(event) {
+  if (choicesBtn.textContent === questions.answer) {
+    score+=20
+    console.log(score)
+  };
+
+
+}
