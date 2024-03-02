3df998f8def3ccfc53cb55a0cb210bc3b88881cb
Jinkc21 <jin.k.choe@gmail.com>
2024-03-01 19:46:37 -0500

    added resluts section and updated timer function

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
+
+## User Story
+
+```
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+```
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and my score
+```
+
+ToDo: 
+-fix point system
+-update timer to subtract seconds
+-log scores and initials to local storage
+-style page better
+-remove ability to click other buttons after answering
+-
+
+
+
+
+
 # w4c-code-quiz
+Code Quiz - Javascript
+
+
+
+
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+Quiz template and tutorial was provided by Web Dev Simplified on YouTube:
+https://www.youtube.com/watch?v=riDzcEQbX6k
+
+MDM Web Docs helped provide information to create the timer function:
+https://developer.mozilla.org/en-US/docs/Web/API/setInterval
+https://developer.mozilla.org/en-US/docs/Web/API/clearInterval
+
+
+Quiz questions were provided by Laurence Svekis on LinkedIn:
+https://www.linkedin.com/pulse/ultimate-javascript-quiz-over-200-questions-test-your-svekis--eilzc/
+
+## License
+
+The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+
+---
--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,42 @@
 const startBtn = document.getElementById("start-btn")
 const nextBtn = document.getElementById("next-btn")
+const submitBtn = document.getElementById("submit-btn")
 const questionCtnr = document.getElementById('question-container')
 const questionEl = document.getElementById('question')
 const answerBtn = document.getElementById('answer-btns')
+const score = document.getElementById('score')
+const highScore = document.getElementById('high-score')
+const totalScore = document.getElementById('total-score')
+const quizHeader = document.getElementById('quiz-header')
+const results = document.getElementById('results')
 
 
 
-let shuffledQuestions, currentQuestions
+const timer = document.getElementById("timer");
+var seconds = 100;
+var scoreCount = 0;
+var timeInterval;
+var timeElapsed = 0;
+
+function countdown() {
+   timeInterval = setInterval(function () {
+    timer.innerHTML = `${seconds} seconds remaining`;
+    seconds--;
+    timeElapsed++;
+
+    if (seconds === 0) {
+      clearInterval(timeInterval);
+      alert("You are out of time!")
+    }
+  }, 1000)
+}
+
+function stopTimer() {
+  clearInterval(timeInterval);
+}
+
+let shuffledQuestions
+let currentQuestions = 0
 
 startBtn.addEventListener('click', startQuiz)
 nextBtn.addEventListener('click', () => {
@@ -16,10 +46,12 @@ nextBtn.addEventListener('click', () => {
 
 
 function startQuiz() {
+  countdown();
   startBtn.classList.add('hide')
   shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestions = 0
   questionCtnr.classList.remove('hide')
+  quizHeader.classList.remove('hide')
   setNext()
 }
 
@@ -35,7 +67,7 @@ function showQuestion(question) {
     button.innerText = answer.text
     button.classList.add('btn')
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
     button.addEventListener('click', selectAnswer)
     answerBtn.appendChild(button)
@@ -51,126 +83,194 @@ function resetState() {
 }
 
 function selectAnswer(e) {
+  var answer = questions[currentQuestions].answers
+  // console.log(answer)
   const selectedBtn = e.target
   const correct = selectedBtn.dataset.correct
   setStatusClass(document.body, correct)
   Array.from(answerBtn.children).forEach(button => {
     setStatusClass(button, button.dataset.correct)
   })
-  if(shuffledQuestions.length > currentQuestions + 1){
-      nextBtn.classList.remove('hide')
-  } else {
-    startBtn.innerHTML = 'Restart'
-    startBtn.classList.remove('hide')
-  }
+  for (var i = 0; i < answer.length; i++){
+    console.log(answer[i])
 
+  //   if (answer.correct) {
+  //   console.log(answer.text)
+  //   button.dataset.correct = answer.correct;
+  //   scoreCount += 10;
+  //   score.innerHTML = scoreCount;
+  // }
+  // else {
+  //   scoreCount -= 10;
+  //   seconds -= 5
+  // }
+  
+  }
+  if (shuffledQuestions.length > currentQuestions + 1) {
+    nextBtn.classList.remove('hide');
+
+  } else {
+    stopTimer();
+    submitBtn.classList.remove('hide');
+    results.classList.remove('hide');
+
+    
+  }
 }
 
+
+function submitQuiz() {
+}
 
 
 function setStatusClass(element, correct) {
   clearStatusClass(element)
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else {
-    element.classList.add('incorrect')
+    element.classList.add('incorrect');
   }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('incorrect')
+  element.classList.remove('correct');
+  element.classList.remove('incorrect');
 }
 
 const questions = [
   {
-    question: "What does CSS stand for?",
+    question: "What is the correct syntax for referring to an external script called 'app.js'?",
     answers: [
-      { text: "create specific sample", correct: false },
-      { text: "cascading style sheets", correct: true },
-      { text: "copy selected string", correct: false },
-      { text: "console sources security", correct: false }
+      { text: "<script href='app.js'>", correct: false },
+      { text: "<script src='app.js'>", correct: true },
+      { text: "<script name='app.js'>", correct: false },
+      { text: "<script file='app.js'>", correct: false }
     ]
   },
   {
-    question: "What tag is used to render or transform text into a italics?",
+    question: "How do you create a function in JavaScript?",
     answers: [
-      { text: "<strong>", correct: false },
-      { text: "<em>", correct: true },
-      { text: "<br>", correct: false },
-      { text: "<ital>", correct: false }
+      { text: "function = myFunction()", correct: false },
+      { text: "function myFunction()", correct: true },
+      { text: "function:myFunction()", correct: false },
+      { text: "create myFunction()", correct: false }
     ]
   },
-  {
-    question: "Which of the following is NOT a commonly used element in HTML?",
-    answers: [
-      { text: "<body>", correct: false },
-      { text: "<title>", correct: false },
-      { text: "<meta>", correct: false },
-      { text: "<page>", correct: true }
-    ]
-  },
-  {
-    question: "Which of the following can be used in css to show a list in a line?",
-    answers: [
-      { text: "display: block", correct: false },
-      { text: "position: straight", correct: false },
-      { text: "display: flex", correct: true },
-      { text: "position: absolute", correct: false }
-    ]
-  },
+  // {
+  //   question: "Which method is used to add an element at the end of an array?",
+  //   answers: [
+  //     { text: "array.pop(element)", correct: false },
+  //     { text: "array.unshift(element)", correct: false },
+  //     { text: "array.shift(element)", correct: false },
+  //     { text: "array.push(element)", correct: true }
+  //   ]
+  // },
+  // {
+  //   question: "How can a JavaScript variable be declared conditionally?",
+  //   answers: [
+  //     { text: "var x if (condition)", correct: false },
+  //     { text: "if (condition) var x", correct: false },
+  //     { text: "var x = (condition) ? value1 : value2", correct: true },
+  //     { text: "condition(var x)", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "How do you write a conditional statement for executing some statements only if i is NOT equal to 5?",
+  //   answers: [
+  //     { text: "if (i <> 5)", correct: false },
+  //     { text: "if i =! 5 then", correct: false },
+  //     { text: "if (i != 5)", correct: true },
+  //     { text: "if (i !== 5)", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "Which operator is used to assign a value to a variable?",
+  //   answers: [
+  //     { text: "*", correct: false },
+  //     { text: "-", correct: false },
+  //     { text: "=", correct: true },
+  //     { text: "+", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "Which function of an Array object calls a function for each element in the array?",
+  //   answers: [
+  //     { text: "forEach()", correct: true },
+  //     { text: "every()", correct: false },
+  //     { text: "forEvery()", correct: false },
+  //     { text: "each()", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "What is the correct way to write a JavaScript array?",
+  //   answers: [
+  //     { text: "var colors = (1:'red', 2:'green', 3:'blue')", correct: false },
+  //     { text: "var colors = 1 = ('red'), 2 = ('green'), 3 = ('blue')", correct: false },
+  //     { text: "var colors = 'red', 'green', 'blue'", correct: false },
+  //     { text: "var colors = ['red', 'green', 'blue']", correct: true }
+  //   ]
+  // },
+  // {
+  //   question: "Which method converts JSON data to a JavaScript object?",
+  //   answers: [
+  //     { text: "JSON.toString()", correct: false },
+  //     { text: "JSON.toObject()", correct: false },
+  //     { text: "JSON.parse()", correct: true },
+  //     { text: "JSON.stringify()", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "How does a for loop start?",
+  //   answers: [
+  //     { text: "for (i = 0; i <= 5)", correct: false },
+  //     { text: "for (i <= 5; i++)", correct: false },
+  //     { text: "for (i = 0; i <= 5; i++)", correct: true },
+  //     { text: "for (i++)", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "What does the this keyword refer to in a JavaScript method?",
+  //   answers: [
+  //     { text: "The global object", correct: false },
+  //     { text: "The object that owns the method", correct: true },
+  //     { text: "The document object", correct: false },
+  //     { text: "The window object", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "Which event occurs when the user clicks on an HTML element?",
+  //   answers: [
+  //     { text: "onchange", correct: false },
+  //     { text: "onclick", correct: true },
+  //     { text: "onmouseover", correct: false },
+  //     { text: "onmouseclick", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "Which method is used to serialize an object into a JSON string in JavaScript?",
+  //   answers: [
+  //     { text: "JSON.stringify()", correct: true },
+  //     { text: "JSON.parse()", correct: false },
+  //     { text: "JSON.toObject()", correct: false },
+  //     { text: "JSON.toString()", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "Which HTML attribute is used to define inline JavaScript?",
+  //   answers: [
+  //     { text: "script", correct: true },
+  //     { text: "js", correct: false },
+  //     { text: "src", correct: false },
+  //     { text: "style", correct: false }
+  //   ]
+  // },
+  // {
+  //   question: "How do you find the length of a string str in JavaScript?",
+  //   answers: [
+  //     { text: "str.length()", correct: false },
+  //     { text: "str.size()", correct: false },
+  //     { text: "str.length", correct: true },
+  //     { text: "length(str)", correct: false }
+  //   ]
+  // },
 ]
-
-// const timer = document.getElementById("timer");
-// var seconds = 60;
-// var score = 0;
-
-
-
-// // console.log(questions[0].question)
-
-// startBtn.addEventListener("click", startQuiz)
-
-
-// function countdown() {
-//   const timeInterval = setInterval(function () {
-//     timer.innerHTML = `${seconds} remaining`;
-//     seconds--;
-
-//     if (seconds === 0) {
-//       clearInterval(timeInterval);
-//       alert("You are out of time!")
-//     }
-//   }, 1000)
-
-// }
-
-
-// function startQuiz() {
-//   document.getElementById("main").style.display='none'
-//   countdown();
-//   showQuiz();
-// }
-// function showQuiz(){
-//   var question = document.createElement("h1")
-//   question.textContent= questions[0].question
-//   document.getElementById('quiz').appendChild(question)
-// for (var i=0; i<questions[0].choices.length; i++){
-//   // console.log(questions[0].choices[i])
-//   var choicesBtn = document.createElement("button")
-//   choicesBtn.textContent= questions[0].choices[i]
-//   choicesBtn.onclick = function(event){
-//     // console.log("hello")
-//     checkAnswer(event)
-//   }
-//   document.getElementById('quiz').appendChild(choicesBtn)
-// }
-
-// }
-// function checkAnswer(event) {
-//   if (choicesBtn.textContent === questions.answer) {
-//     score+=20
-//     console.log(score)
-//   };
-
-// }
